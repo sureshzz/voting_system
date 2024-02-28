22f8727389e2b9c23aa313d33ba85f50d480a758
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-02-28 23:24:31 +0545

    feat: candidates registration

--- voting_system/users/views.py
+++ voting_system/users/views.py
@@ -137,52 +137,52 @@ def vote(request):
             print(result)
             return JsonResponse({"status": "200"})
 
-# @csrf_exempt
-# def cregister(request):   
-#     if request.method == 'POST':
-#         print(request, request.method)
-#         # Retrieve data from POST request
-#         try:
-#             data = json.loads(request.body)
-#             print(data)
-#             name = data.get('name')
-#             flag = data.get('flag')
-#         except json.JSONDecodeError:
-#             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+@csrf_exempt
+def cregister(request):   
+    if request.method == 'POST':
+        print(request, request.method)
+        # Retrieve data from POST request
+        try:
+            data = json.loads(request.body)
+            print(data)
+            name = data.get('name')
+            flag = data.get('flag')
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
 
-#         # Check if required fields are present
-#         if name is None or flag is None:
-#             return JsonResponse({'error': 'Missing required fields'}, status=400)
+        # Check if required fields are present
+        if name is None or flag is None:
+            return JsonResponse({'error': 'Missing required fields'}, status=400)
         
-#         # Create dictionary with user data
-#         user_data = {'name': name, 'flag': flag}
+        # Create dictionary with user data
+        user_data = {'name': name, 'flag': flag}
         
-#         # Insert data into MongoDB collection
-#         candidates_collection.insert_one(user_data)
+        # Insert data into MongoDB collection
+        candidates_collection.insert_one(user_data)
         
-#         return JsonResponse({'message': 'candidate registered successfully'}, status=201)
+        return JsonResponse({'message': 'candidate registered successfully'}, status=201)
     
-#     else:
-#         return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
+    else:
+        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
     
 
-# import json
+import json
 
-# def cinfo(request):
-#     if request.method == 'GET':
-#         # Retrieve all documents from the users collection
-#         all_documents = candidates_collection.find()
+def cinfo(request):
+    if request.method == 'GET':
+        # Retrieve all documents from the users collection
+        all_documents = candidates_collection.find()
 
-#         # Convert documents to a list of dictionaries
-#         document_list = [document for document in all_documents]
-#         print(document_list)
+        # Convert documents to a list of dictionaries
+        document_list = [document for document in all_documents]
+        print(document_list)
 
-#         # Convert ObjectId to string in each document
-#         for document in document_list:
-#             document['_id'] = str(document['_id'])
+        # Convert ObjectId to string in each document
+        for document in document_list:
+            document['_id'] = str(document['_id'])
 
-#         # Serialize the document list to JSON
-#         json_data = json.dumps(document_list)
+        # Serialize the document list to JSON
+        json_data = json.dumps(document_list)
 
-#         # Return JSON response with the serialized data
-#         return JsonResponse(json_data, safe=False)
+        # Return JSON response with the serialized data
+        return JsonResponse(json_data, safe=False)
